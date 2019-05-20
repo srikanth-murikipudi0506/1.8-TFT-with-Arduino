1ed9f88123762be66669b19222ae87a409a62f45
srikanth murikipudi <50315236+srikanth-murikipudi0506@users.noreply.github.com>
2019-05-20 16:40:13 +0530

    Add files via upload

---
+++ Sample.ino
@@ -0,0 +1,41 @@
+// include TFT and SPI libraries
+#include <TFT.h>  
+#include <SPI.h>
+
+// pin definition for Arduino UNO
+#define cs   10
+#define dc   9
+#define rst  8
+
+
+// create an instance of the library
+TFT TFTscreen = TFT(cs, dc, rst);
+
+void setup() {
+
+  //initialize the library
+  TFTscreen.begin();
+
+  // clear the screen with a black background
+  TFTscreen.background(255, 255,255);
+  //set the text size
+  TFTscreen.setTextSize(3);
+}
+
+void loop() {
+
+  //generate a random color
+  int redRandom = random(0, 255);
+  int greenRandom = random (0, 255);
+  int blueRandom = random (0, 255);
+  
+  // set a random font color
+  TFTscreen.stroke(redRandom, greenRandom, blueRandom);
+  
+  // print Hello, World! in the middle of the screen
+  TFTscreen.text("MbedApps !!", 6, 57);
+  
+  // wait 200 miliseconds until change to next color
+  delay(200);
+}
+
